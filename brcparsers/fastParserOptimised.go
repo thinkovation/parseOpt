eb7cc2880b114dffa6b28d2196b376a6c2eeeeab
Gary Barnett <gary@thinkovation.com>
2024-03-04 15:22:35 +0000

    Update Readme, add a couple of csv's and liberally comment the functions

--- brcparsers/fastParserOptimised.go
+++ brcparsers/fastParserOptimised.go
@@ -3,21 +3,35 @@ package brcparsers
 // FastParserOptimised adapts the version created at https://github.com/shraddhaag/1brc/blob/main/main.go by swapping the
 // switch out for an if else
 func FastParserOptimised(input string) (int64, error) {
+	// Variable to track if the number is negative
 	var isNegativeNumber bool
+
+	// Check if the input string starts with a negative sign
 	if input[0] == '-' {
 		isNegativeNumber = true
+		// Remove the negative sign from the input
 		input = input[1:]
 	}
-	var output int64
-	if len(input) == 3 {
 
+	// Variable to store the parsed integer value
+	var output int64
+
+	// Determine the length of the input string to decide the parsing logic
+	if len(input) == 3 {
+		// If the input string has length 3, parse accordingly
+		// Parse the first and third characters and subtract the ASCII value of '0' multiplied by 11
 		output = int64(input[0])*10 + int64(input[2]) - int64('0')*11
 	} else {
+		// If the input string has length other than 3, parse accordingly
+		// Parse the first, second, and fourth characters and subtract the ASCII value of '0' multiplied by 111
 		output = int64(input[0])*100 + int64(input[1])*10 + int64(input[3]) - (int64('0') * 111)
 	}
 
+	// If the number was negative, return the negation of the parsed value
 	if isNegativeNumber {
 		return -output, nil
 	}
+
+	// Otherwise, return the parsed value
 	return output, nil
 }
--- brcparsers/standardParser.go
+++ brcparsers/standardParser.go
@@ -2,13 +2,15 @@ package brcparsers
 
 import "strconv"
 
+// StandardParseFloat parses a string representation of a floating-point number into an integer value.
+// It multiplies the parsed floating-point number by 10 and returns the resulting integer value.
 func StandardParseFloat(input string) (int64, error) {
-
+	// Parse the input string as a floating-point number with 64-bit precision
 	f, err := strconv.ParseFloat(input, 64)
 	if err != nil {
 		return 0, err // Return an error if conversion fails
 	}
 
+	// Convert the parsed floating-point number to an integer by multiplying it by 10
 	return int64(f * 10), nil
-
 }
